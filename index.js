7ce976ecec104fc3ab833f1c37f3992db48a156e
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2022-09-11 15:49:11 +0530

    Validation Form Example using HTML CSS and JS

---
+++ index.js
@@ -0,0 +1,67 @@
+let userForm = document.getElementById("validation-form");
+
+const retrieveEntries = () => {
+  let entries = localStorage.getItem("user-entries");
+  if (entries) {
+    entries = JSON.parse(entries);
+  } else {
+    entries = [];
+  }
+  return entries;
+};
+
+let userEntries = [];
+
+const displayEntries = () => {
+  const entries = retrieveEntries();
+  const tableEntries = entries
+    .map((entry) => {
+      const nameCell = `<td>${entry.name}</td>`;
+      const emailCell = `<td>${entry.email}</td>`;
+      const passwordCell = `<td>${entry.password}</td>`;
+      const dobCell = `<td>${entry.dob}</td>`;
+      const acceptTermsCell = `<td>${entry.acceptedTermsAndConditions}`;
+
+      const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+      return row;
+    })
+    .join("\n");
+
+  const table = `<table class="table">
+    <tr>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Password</th>
+    <th>dob</th>
+    <th>accepted terms</th>
+    </tr>${tableEntries}</table>`;
+
+  let details = document.getElementById("user-entries");
+  details.innerHTML = table;
+};
+
+const saveUserForm = (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const dob = document.getElementById("date").value;
+  const acceptedTermsAndConditions =
+    document.getElementById("acceptedTerms").checked;
+
+  const entry = {
+    name,
+    email,
+    password,
+    dob,
+    acceptedTermsAndConditions,
+  };
+
+  userEntries.push(entry);
+
+  localStorage.setItem("user-entries", JSON.stringify(userEntries));
+  displayEntries();
+};
+
+userForm.addEventListener("submit", saveUserForm);
+displayEntries();
